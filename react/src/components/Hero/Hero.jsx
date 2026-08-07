21b73b44c5bdaf394e1e531561e1214a74cace15
Aziz2k618 <24-CS-122@students.uettaxila.edu.pk>
2026-08-08 00:09:41 +0500

    Create Reusable Buttton Component

--- react/src/components/Hero/Hero.jsx
+++ react/src/components/Hero/Hero.jsx
@@ -1,15 +1,21 @@
-
+import {useState} from "react";
+import Button from "../Button/Button";
 
 export default function Hero({title, description, button1, button2}){
+  const [message, setMessage] = useState("");
+  function handleClick(){
+    alert("Button clicked!");
+  }
   return(
     <div className="hero">
       <section>
        <h1>{title}</h1>
 
        <p>{description}</p>
+       {message && <p>{message}</p>}
 
-       <button>{button1}</button>
-       <button>{button2}</button>
+       <Button onClick={handleClick}>{button1}</Button>
+       <Button onClick={()=>setMessage("Thanks for checking out our projects!")}>{button2}</Button>
 
       </section>  
     </div>
